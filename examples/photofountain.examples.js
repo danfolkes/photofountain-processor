c87cea36c44f14e5254ec055dbf091d52e4ede2f
dan@danfolkes.com <dan@danfolkes.com>
2012-11-12 17:01:54 -0500

    blah

--- examples/photofountain.examples.js
+++ examples/photofountain.examples.js
@@ -3,3 +3,17 @@
 	https://danfolkes.com/photofountain/examples.php
 	
 */
+
+
+$(document).ready(function() {
+	
+	Pixastic.process(
+	  this,
+	  "brightness",	// brightness/contrast adjustment
+	  {
+		"brightness" : 100,
+		"contrast" : 0.5
+	  }
+	)
+  
+});
