91807a7ad1d880889275c0a1af9d7a4b0641532e
dan@danfolkes.com <dan@danfolkes.com>
2012-11-12 16:22:24 -0500

    almost

--- photofountain.full.js
+++ photofountain.full.js
@@ -441,6 +441,7 @@ function loadjsfile(filename){
   var fileref=document.createElement('script');
   fileref.setAttribute("type","text/javascript");
   fileref.setAttribute("src", filename);
+  document.getElementsByTagName("head")[0].appendChild(fileref)
 }
 
 
